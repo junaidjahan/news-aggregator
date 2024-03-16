edddacada9ae5c67131d1c16123c3dd77384c24d
junaidjahan <70884217+junaidjahan@users.noreply.github.com>
2024-03-16 04:22:56 -0700

    chore:  ➕add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './assets/styles/index.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router/index.tsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
