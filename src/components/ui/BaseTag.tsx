3faf9fe975ef9f83c329c81677243edd65d3e003
junaidjahan <70884217+junaidjahan@users.noreply.github.com>
2024-03-18 08:57:24 -0700

    feat:  🏗️ add ui components

---
+++ src/components/ui/BaseTag.tsx
@@ -0,0 +1,27 @@
+
+type Color = 'primary' | 'secondary' | 'gray'
+
+interface TagProps {
+    title:string;
+    color:Color
+}
+
+export const BaseTag = (props:TagProps) => {
+    const tagColor = {
+        primary :'bg-primary-100 text-primary-700',
+        secondary:'bg-secondary-100 text-cyan-700',
+        gray: 'bg-gray-100 text-gray-700'
+    } 
+
+
+
+    return (
+        <>
+            <div className={`${tagColor[props.color]} w-fit p-1 px-2.5 text-xs rounded-full`}>
+                <h1>
+                    {props.title}
+                </h1>
+            </div>
+        </>
+    )
+}
---
+++ src/components/ui/index.ts
@@ -0,0 +1,4 @@
+export * from './BaseTextfield.tsx'
+export * from './BaseSelect.tsx'
+export * from './BaseDatePicker.tsx'
+export * from './BaseTag.tsx'
